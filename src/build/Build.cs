6b66119e94ac1a890a240cb12c3bde445d8064a3
rflechner <rflechner@romcyber.com>
2024-12-01 14:45:36 +0100

    devops: update build

--- src/build/Build.cs
+++ src/build/Build.cs
@@ -23,7 +23,7 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
     
-    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath ArtifactsDirectory => RootDirectory / ".." / "artifacts";
 
     AbsolutePath CoreLibProject => RootDirectory / "EasyParsing" / "EasyParsing.csproj";
     AbsolutePath JsonParserProject => RootDirectory / "EasyParsing.Samples.Json" / "EasyParsing.Samples.Json.csproj";
